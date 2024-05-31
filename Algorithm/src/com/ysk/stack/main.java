2a77033b42e28be514585aab6b857227a76ad146
ysevenk <3275223747@qq.com>
2024-05-31 17:19:05 +0800

    update:栈

--- Algorithm/src/com/ysk/stack/main.java
+++ Algorithm/src/com/ysk/stack/main.java
@@ -7,5 +7,21 @@ public class main {
     public static void main(String[] args) {
         Stack<String> stack = new Stack<>();
         // System.out.println(stack.empty());
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        stack.push("4");
+
+        // 将栈顶元素从栈中删除
+        // stack.pop();
+        // stack.pop();
+        // stack.pop();
+        String num = stack.pop();
+
+        // System.out.println(stack);
+        // System.out.println(num);
+        // System.out.println(stack.peek());
+        System.out.println(stack.search("2"));
+
     }
 }
