563b7cd6b7ff6460d3e49e3e178e23a2fa48b468
ysevenk <3275223747@qq.com>
2024-06-02 20:35:02 +0800

    update:队列

---
+++ Algorithm/src/com/ysk/queue/main.java
@@ -0,0 +1,20 @@
+package com.ysk.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class main {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer("a");
+        queue.offer("b");
+        queue.offer("c");
+        queue.offer("d");
+
+        // System.out.println(queue.peek());
+        queue.poll(); // 取出并删除一个元素
+        // queue.poll(); // 取出并删除一个元素
+        System.out.println(queue);
+        System.out.println(queue.contains("b"));
+    }
+}
