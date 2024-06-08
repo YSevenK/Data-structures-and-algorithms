7d0b4c76a10b0a075ced908283a9f4b4b35db5d5
ysevenk <3275223747@qq.com>
2024-06-08 11:13:21 +0800

    update:插入排序

---
+++ Algorithm/src/com/ysk/InsertionSort/main.java
@@ -0,0 +1,27 @@
+package com.ysk.InsertionSort;
+
+public class main {
+    public static void main(String[] args) {
+
+        int[] array = {12, 11, 13, 5, 6};
+
+        insertionSort(array);
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+    }
+}
