57056293ca571cd83e4f0cecb02920ba024cd8b1
ysevenk <ysevenk@qq.com>
2024-12-16 13:05:41 +0800

    printStart

---
+++ Algorithm/src/com/Demo/BaldPyramid.java
@@ -0,0 +1,17 @@
+package com.Demo;
+
+public class BaldPyramid {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {// 打空格
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < i + 2; k++) {// 打*号
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Algorithm/src/com/Demo/PrintStart.java
@@ -0,0 +1,17 @@
+package com.Demo;
+
+public class PrintStart {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
