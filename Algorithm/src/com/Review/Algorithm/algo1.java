bb9206eadc51cdfda76025654aee446cf4b4154a
ysevenk <ysevenk@qq.com>
2024-12-26 12:16:31 +0800

    bubble

--- Algorithm/src/com/Review/Algorithm/algo1.java
+++ Algorithm/src/com/Review/Algorithm/algo1.java
@@ -5,7 +5,7 @@ package com.Review.Algorithm;
  */
 public class algo1 {
     public static void bubble(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
@@ -19,12 +19,12 @@ public class algo1 {
     public static void main(String[] args) {
         int[] arr = {9, 7, 4, 6, 3, 1, 10};
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(i == 0 ? +arr[i] : "," + arr[i]);
+            System.out.print(i == 0 ? arr[i] : "," + arr[i]);
         }
         bubble(arr);
         System.out.println();
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(i == 0 ? +arr[i] : "," + arr[i]);
+            System.out.print(i == 0 ? arr[i] : "," + arr[i]);
         }
     }
 }
