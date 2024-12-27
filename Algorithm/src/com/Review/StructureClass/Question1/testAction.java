2786ed3726ea24dcb24a6d93ba050ad5d2ad6448
ysevenk <ysevenk@qq.com>
2024-12-27 09:45:04 +0800

    review!!

--- Algorithm/src/com/Review/StructureClass/Question1/testAction.java
+++ Algorithm/src/com/Review/StructureClass/Question1/testAction.java
@@ -1,18 +1,22 @@
 package com.Review.StructureClass.Question1;
 
-public class testAction implements Action {
-    @Override
-    public void act() {
-        System.out.println("实现类：act方法");
-    }
-}
-
-class Main {
+public class testAction {
     public static void main(String[] args) {
+        // 通过匿名内部类实现Action接口
+        Action action = new Action() {
+            @Override
+            public void act() {
+                System.out.println("匿名内部类实现 act()");
+            }
+        };
+
+        // 调用抽象方法act，此时执行的是匿名内部类中重写的act方法
+        action.act();
+
+        // 调用静态方法info，通过接口名直接调用
         Action.info();
 
-        testAction action = new testAction();
-        action.act();
+        // 调用默认方法shout，此时执行的是匿名内部类从接口继承来的默认方法shout
         action.shout();
     }
 }
