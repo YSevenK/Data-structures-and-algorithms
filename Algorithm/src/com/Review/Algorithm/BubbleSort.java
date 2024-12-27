0803eb1d4b687ce36494d8dbcc084e81029f6e2a
ysevenk <ysevenk@qq.com>
2024-12-27 11:54:24 +0800

    bubble

--- Algorithm/src/com/Review/Algorithm/BubbleSort.java
+++ Algorithm/src/com/Review/Algorithm/BubbleSort.java
@@ -5,8 +5,7 @@ public class BubbleSort {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp;
-                    temp = arr[j];
+                    int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
@@ -22,10 +21,10 @@ public class BubbleSort {
             System.out.print(i == 0 ? arr[i] : "," + arr[i]);
         }
         System.out.println();
-        int[] sortedArray = bubbleSort(arr);
+        bubbleSort(arr);
         System.out.print("冒泡排序后(从小到大):");
-        for (int i = 0; i < sortedArray.length; i++) {
-            System.out.print(i == 0 ? sortedArray[i] : "," + sortedArray[i]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(i == 0 ? arr[i] : "," + arr[i]);
         }
     }
 }
